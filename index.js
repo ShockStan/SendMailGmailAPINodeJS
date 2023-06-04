1bcd257f27e34c3dea40382d24f834fe8b34b5b6
ShockStan <aaaeroa@gmail.com>
2023-06-04 23:18:17 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const process = require('process')
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis')
 
-const SCOPES = ['https://mail.google.com/'];
+const SCOPES = ['https://mail.google.com/']; 
 
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const TOKEN_PATH = path.join(process.cwd(), 'token.json'); //a token.json file is created if the app is not authorised, and after first time authorisation tokens are used from token.json
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json'); //credentials downloaded from google.cloud console are stored as credentials.json
 
-const saveCredential = async(user)=>{
+const saveCredential = async(user)=>{ //method to create token.json file which will store credentials for authorisation
     const credentialsJSON = await fs.readFile(CREDENTIALS_PATH);
     const keys = JSON.parse(credentialsJSON);
     const key = keys.installed;
@@ -22,7 +22,7 @@ const saveCredential = async(user)=>{
     await fs.writeFile(TOKEN_PATH, payLoad);
 }
 
-const loadCredentials = async()=>{
+const loadCredentials = async()=>{ //method to load stored credentials from token.json
     try{
         const token = await fs.readFile(TOKEN_PATH)
         const credentials = JSON.parse(token)
@@ -30,7 +30,7 @@ const loadCredentials = async()=>{
     }catch(err){return null;}
 }
 
-const authorize = async ()=>{
+const authorize = async ()=>{ //method to authorise gmail sign in
     let user = await loadCredentials();
     if(user){return user;}
     user = await authenticate({
@@ -42,114 +42,116 @@ const authorize = async ()=>{
     return user;
 }
 
-const findToAddress = async(str)=>{
+const findToAddress = async(str)=>{ //method to find the mail id to which reply has to be found. the initial id is in form <random@mail.com>
     const a = str.indexOf('<');
     const b = str.indexOf('>');
     return str.substring(a+1,b);
 }
 
-const ReplyMessage = 'This is an automated Reply. I am on Vacation. Will reply when I get back to Work. Cheers!!!';
-const LABELName = 'AutomatedReplies';
+const ReplyMessage = 'This is an automated Reply. I am on Vacation. Will reply when I get back to Work. Cheers!!!'; //reply msg to be sent
+const LABELName = 'AutomatedReplies'; //label name 
 
-const main = async ()=>{
-    const auth = await authorize();
-    const emailList = await ListEmailIds(auth);
-    const idList = emailList.map(a=>a.id);
-    const allEmails = await getAllEmails(auth, idList);
-    const threadIDofSentMails = allEmails[1].map(a=>a.data.threadId);
-    for(let i=0;i<allEmails[0].length;i++){
-        let thread_id = allEmails[0][i].data.threadId;
-        let mail_id = allEmails[0][i].data.id;
-        if(!threadIDofSentMails.includes(thread_id)){
-            let ToAddress = await findToAddress(allEmails[0][i].data.payload.headers.filter(p=>p.name==='From')[0].value);
-            const ReplySent = await sendReply(auth, ReplyMessage, ToAddress, thread_id);
-            threadIDofSentMails.push(thread_id);
-            const Label = await modifyLabels(auth, mail_id, LABELName);
+const main = async ()=>{ //main method
+    const auth = await authorize(); //authorisation
+    const emailList = await ListEmailIds(auth); //list of ids and threadIds of all mails are found
+    const idList = emailList.map(a=>a.id); //ids are separeated from threadIds
+    const allEmails = await getAllEmails(auth, idList); //emails in inbox and sent are got as an array
+    const threadIDofSentMails = allEmails[1].map(a=>a.data.threadId); //threadId of all mails in SENT are got as an array
+    for(let i=0;i<allEmails[0].length;i++){ //for loop to find mails which havent still got replies
+        let thread_id = allEmails[0][i].data.threadId; //threadId of mail is found
+        let mail_id = allEmails[0][i].data.id; //id of mail is found
+        if(!threadIDofSentMails.includes(thread_id)){ //whether threadId is found in SENT mails threadIds array or not is found
+            let ToAddress = await findToAddress(allEmails[0][i].data.payload.headers.filter(p=>p.name==='From')[0].value); //if not, to mail address is found
+            const ReplySent = await sendReply(auth, ReplyMessage, ToAddress, thread_id); //reply is sent
+            threadIDofSentMails.push(thread_id); //the thread id is addes list of thread id for which reply is sent
+            const Label = await modifyLabels(auth, mail_id, LABELName); //the label is attached the mail from inbox
         }
     }
 }
 
-setInterval(()=>{main()},60000);
+setInterval(()=>{main()},90000); //program to set to run at intervak of 1 and half min
 
-const ListEmailIds = async (auth)=>{
-    const gmail = google.gmail({version: 'v1', auth});
-    const response = await gmail.users.messages.list({
+//userId: 'me' ---> means that the process is done for my own email id with which i have authorised
+
+const ListEmailIds = async (auth)=>{ //method to get list of email ids
+    const gmail = google.gmail({version: 'v1', auth}); 
+    const response = await gmail.users.messages.list({ //users.messages.list 
         userId: 'me',
     })
     const label = response.data;
     return label.messages;
 }
 
-const getAllEmails = async (auth, idArray)=>{
+const getAllEmails = async (auth, idArray)=>{ //method to find all emails in INBOX & SENT and return as array
     const gmail = google.gmail({version: 'v1', auth});
     const array = []
     for(let i=0;i<idArray.length;i++){
-        const response = await gmail.users.messages.get({
+        const response = await gmail.users.messages.get({ //users.messages.get
             userId: 'me',
             id: idArray[i],
         })
         array.push(response)
     }
-    const mailsInInbox = [...array.filter(a=>a.data.labelIds.includes('INBOX'))];
-    const mailsInSent = [...array.filter(a=>a.data.labelIds.includes('SENT'))];
+    const mailsInInbox = [...array.filter(a=>a.data.labelIds.includes('INBOX'))];//emails with INBOX in labelIds
+    const mailsInSent = [...array.filter(a=>a.data.labelIds.includes('SENT'))];//emails with SENT in labelIds
     const returnArray = [];
-    returnArray.push(mailsInInbox, mailsInSent);
+    returnArray.push(mailsInInbox, mailsInSent);//INBOX is kept at 0th index and SENT is kept at 1st index
     return returnArray;
 }
 
-const sendReply = async (auth, mailBody, mailTo, threadID)=>{
+const sendReply = async (auth, mailBody, mailTo, threadID)=>{ //method to send reply mail
     const gmail = google.gmail({version:'v1',auth});
-    const message = [`To:${mailTo}`,'Subject:AutomaticRepl','Content-Type: text/plain; charset=utf-8','',mailBody];
-    const rawEmail = Buffer.from(message.join('\n').trim()).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-    const response = await gmail.users.messages.send({
+    const message = [`To:${mailTo}`,'Subject:AutomaticReply','Content-Type: text/plain; charset=utf-8','',mailBody];
+    const rawEmail = Buffer.from(message.join('\n').trim()).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''); //email is converted to baseb4 and '+'&'/' are replaced with '-'&'_' respectively. '==' at start & end are replaced with nothing
+    const response = await gmail.users.messages.send({ //users.messages.send
         userId: 'me',
         requestBody:{
-            raw: rawEmail,
-            threadId: threadID
+            raw: rawEmail, //email contents
+            threadId: threadID //id of thread to which the reply is to be sent
         }
     });
-    const date = new Date();
-    console.log(`Automated reply sent to ${mailTo} at ${date}`);
+    const date = new Date();//date for logging purpose
+    console.log(`Automated reply sent to ${mailTo} at ${date}`);//after mail is sent, a success log is created
 }
 
-const checkLabelExist = async(auth, labelName) =>{
+const checkLabelExist = async(auth, labelName) =>{ //method to check if the label to which Mail is to attached exist
     const gmail = google.gmail({version: 'v1', auth});
-    const response = await gmail.users.labels.list({
+    const response = await gmail.users.labels.list({ //users.labels.list
         userId: 'me',
     })
     const labels = response.data.labels;
-    const labelExist = labels.find(a=>a.name===labelName);
-    if(labelExist){
+    const labelExist = labels.find(a=>a.name===labelName); 
+    if(labelExist){//if label with specified name exist, label id is returned
         return labelExist.id;
     }
-    return false;
+    return false; //else false
 }
 
-const createLabel = async(auth, labelName)=>{
+const createLabel = async(auth, labelName)=>{ //method to create label id=f it doesnt exist
     const gmail = google.gmail({version: 'v1', auth});
-    const response = gmail.users.labels.create({
+    const response = gmail.users.labels.create({ //users.labels.create
         userId: 'me',
         requestBody:{
-            name: labelName,
+            name: labelName,//label name
         }
     });
     return response;
 }
 
-const modifyLabels =async(auth, mailID, labelName)=>{
+const modifyLabels =async(auth, mailID, labelName)=>{ //method to modify label of mail with given id
     const gmail = google.gmail({version: 'v1', auth});
-    let labelID = await checkLabelExist(auth, labelName);
+    let labelID = await checkLabelExist(auth, labelName); //whether the label exists or not, is checked
     if(!labelID){
-        const creatingLabel = await createLabel(auth, labelName);
-        labelID = creatingLabel.data.id;
+        const creatingLabel = await createLabel(auth, labelName); //if not a new label is created
+        labelID = creatingLabel.data.id;//label id
     }
-    const response = await gmail.users.messages.modify({
+    const response = await gmail.users.messages.modify({ //users.messages.modify
         userId:'me',
         id:mailID,
         requestBody:{
-            addLabelIds: labelID,
+            addLabelIds: labelID,//labelId is added to array of label ids
         }
     });
-    console.log(`The mail with id ${mailID} is attached with label ${labelName}`);
+    console.log(`The mail with id ${mailID} is attached with label ${labelName}`); //success is logged
     return response;
 }
